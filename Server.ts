31c7c779695047dc65c2b025fb2001312a8c0439
JaquelineSchnitzer <37657835+JaquelineSchnitzer@users.noreply.github.com>
2018-05-13 15:39:22 +0200

    Query Daten im Browser

--- Server.ts
+++ Server.ts
@@ -26,13 +26,14 @@ namespace Server {
         let a: number = parseInt(query["a"]);
         let b: number = parseInt(query["b"]);
 
-        for (let key in query) 
+        for (let key in query)
             console.log(query[key]);
 
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
 
         _response.write("Ich habe dich gehört<br/>");
+        _response.write("Query Daten: " + (query[key]) + "<br>");
         _response.write("Das Ergebnis ist: " + (a + b));
 
         _response.end();
