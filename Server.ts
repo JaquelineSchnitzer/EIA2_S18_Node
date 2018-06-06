354eb4ba5816a993d772fa69f4d19dab340e68d0
JaquelineSchnitzer <37657835+JaquelineSchnitzer@users.noreply.github.com>
2018-06-06 19:36:17 +0200

    Server.ts geändert

--- Server.ts
+++ Server.ts
@@ -10,31 +10,101 @@ namespace Server {
     if (port == undefined)
         port = 8100;
 
-    let server: Http.Server = Http.createServer();
-    server.addListener("listening", handleListen);
-    server.addListener("request", handleRequest);
-    server.listen(port);
-
-    function handleListen(): void {
-        console.log("Ich höre?");
+// Studi Interface
+    interface Studi {
+        name: string;
+        firstname: string;
+        matrikel: number;
+        courseOfStudies: string
+        age: number;
+        gender: boolean;
     }
 
-    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
-        console.log("Ich höre Stimmen!");
+    interface Studis {
+         [matrikel: string]: Studi;
+    }
 
-        let query: AssocStringString = Url.parse(_request.url, true).query;
-        let a: number = parseInt(query["a"]);
-        let b: number = parseInt(query["b"]);
+    let studiHomoAssoc: Studis = {};
 
-        for (let key in query)
-            console.log(query[key]);
-
+// Server
+    let server: Http.Server = Http.createServer((_request: Http.IncomingMessage, _response: Http.ServerResponse) => {
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
+    });
+    server.addListener("request", clientRequest);
+    server.listen(port);
 
-        _response.write("Ich habe dich gehört<br/>");
-        _response.write("Das Ergebnis ist: " + (a + b));
+// Client Request
+    function clientRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+        let clientQuery: AssocStringString = Url.parse(_request.url, true).query;
+        console.log(clientQuery["command"]);
+
+        if (clientQuery["command"] == "insert") {
+            insertRequest(clientQuery, _response);
+        }
+        else if (clientQuery["command"] == "refresh") {
+            refreshRequest(_response);
+        }
+        else if (clientQuery["command"] == "search") {
+             searchRequest(clientQuery, _response);
+        }
+        else {
+             errorHandler();
+        }
 
         _response.end();
+
     }
-}
+
+        function errorHandler(): void {
+            alert("Funktion nicht gefunden!");
+        }
+
+// Insert Request
+        function insertRequest(query: AssocStringString, _response: Http.ServerResponse): void {
+                   let obj: Studi = JSON.parse(query["data"]);
+                   let _name: string = obj.name;
+                   let _firstname: string = obj.firstname;
+                   let matrikel: string = obj.matrikel.toString();
+                   let _courseOfStudies: string = obj.courseOfStudies;
+                   let _age: number = obj.age;
+                   let _gender: boolean = obj.gender;
+                   let studi: Studi;
+            studi = {
+                name: _name,
+                firstname: _firstname,
+                matrikel: parseInt(matrikel),
+                courseOfStudies: _courseOfStudies,
+                age: _age,
+                gender: _gender,
+            };
+            studiHomoAssoc[matrikel] = studi;
+            _response.write("Daten in Datenbank gespeichtert!");
+         }
+
+         function refreshRequest(_response: Http.ServerResponse): void {
+            console.log(studiHomoAssoc);
+            for (let matrikel in studiHomoAssoc) {
+                let studi: Studi = studiHomoAssoc[matrikel];
+                let line: string = matrikel + ": ";
+                line += studi.courseOfStudies + ", " + studi.name + ", " + studi.firstname + ", " + studi.age + " Jahre, ";
+                line += studi.gender ? "Male" : "Female";
+                line += "\n";
+                _response.write(line);
+            }
+          }
+
+                  function searchRequest(query: AssocStringString, _response: Http.ServerResponse): void {
+                      let studi: Studi = studiHomoAssoc[query["searchFor"]];
+                      if (studi) {
+                          let line: string = query["searchFor"] + ": ";
+                          line += studi.courseOfStudies + ", " + studi.name + ", " + studi.firstname + ", " + studi.age + " Jahre ";
+                          line += studi.gender ? "Male" : "Female";
+                          _response.write(line);
+                      }
+                      else {
+                          _response.write("Keine Daten in Datenbank gefunden!");
+                      }
+                  }
+
+          }
